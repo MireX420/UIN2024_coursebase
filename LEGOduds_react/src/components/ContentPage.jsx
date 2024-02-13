1398890c0d44899f41f229720ea5df50c5cc4b2e
MireX420 <adrian_mirek@yahoo.no>
2024-02-13 12:21:41 +0100

    Fikset nav og product card

--- LEGOduds_react/src/components/ContentPage.jsx
+++ LEGOduds_react/src/components/ContentPage.jsx
@@ -1,18 +1,22 @@
-import {products} from '../assets/legoduds'
 import Title from './Title'
 import ProductCard from './ProductCard'
-
-export default function ContentPage({amount, setAmount, category}){
-    return(
+import {products} from '../assets/legoduds'
+import { useParams } from 'react-router-dom'
+export default function ContentPage({amount, setAmount, category, setCart, cart}){
+  const {id} = useParams() 
+  return(
         <main>
-          <Title category={category}/>
+          <Title category={id} />
           {products.map(product => 
-          <ProductCard 
+          <ProductCard
+          cart={cart} 
+          setCart={setCart}
+          prodid={product.prodid}
           key={product.prodid} 
           category={product.category} 
           title={product.title} 
           img={product.imagefile} 
-          price={product.price} 
+          price={product.price}
           amount={amount}
           setAmount={setAmount}
           /> )}
